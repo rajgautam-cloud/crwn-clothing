0ae979b17969bb0a11dbdff4076a596cbd933481
gautamRaj-cloud <raj350199@gmail.com>
2021-09-27 08:41:26 +0530

    added cart-icon and cart-dropdown

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,5 @@
+import CardActionTypes from "./cart.types";
+
+export const toggleCartHidden = () => ({
+  type: CardActionTypes.TOOGLE_CART_HIDDEN,
+});
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,4 @@
+const CardActionTypes = {
+  TOOGLE_CART_HIDDEN: "TOOGLE_CART_HIDDEN",
+};
+export default CardActionTypes;
